21b0c2c9dfdb29a302e81317ce903dc04d0ee16d
athlindemark <78022224+athlindemark@users.noreply.github.com>
2021-09-14 11:11:01 +0200

    changed flaskbank.py to be more similar to flask tutorial

--- flaskbank.py
+++ flaskbank.py
@@ -1,23 +1,46 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', Title='home')
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-@app.route("/about")
-def about():
-    return render_template('about.html', Title='about')
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-@app.route("/login")
-def login():
-    return render_template('login.html', Title='login')
+    @app.route("/")
+    @app.route("/home")
+    def home():
+        return render_template('home.html', Title='home')
 
-@app.route("/register")
-def login():
-    return render_template('register.html', Title='register')
+    @app.route("/about")
+    def about():
+        return render_template('about.html', Title='about')
+
+    @app.route("/login")
+    def login():
+        return render_template('login.html', Title='login')
+
+    @app.route("/register")
+    def login():
+        return render_template('register.html', Title='register')
+
+    return app
 
 
 if __name__ == "__main__":
