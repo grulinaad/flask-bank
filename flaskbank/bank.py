cc09aa1d8c0258bd507df5245120bb2fb1d2eab3
Henrik Aadnoy <259220@uis.no>
2021-09-21 13:53:15 +0200

    added delete

--- flaskbank/bank.py
+++ flaskbank/bank.py
@@ -8,6 +8,8 @@ from flaskbank.db import get_db
 
 bp = Blueprint('bank', __name__)
 
+
+
 @bp.route('/')
 def index():
     db = get_db()
@@ -40,3 +42,13 @@ def create():
             return redirect(url_for('bank.index'))
 
         return render_template('bank/create.html')
+
+
+@bp.route('/int:id>/delete', methods=('POST'))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id, ))
+    db.commit()
+    return redirect(url_for('blog.index'))
