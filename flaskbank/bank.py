c8d313f18d9df0a4ddf6ba3e2dc5977baf84af34
haraldfg <haraldfgrunn@gmail.com>
2021-09-28 10:38:22 +0200

    siste endringer transaction

--- flaskbank/bank.py
+++ flaskbank/bank.py
@@ -52,3 +52,44 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id, ))
     db.commit()
     return redirect(url_for('blog.index'))
+
+@bp.route('/transaction', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        from_account = int(request.form['from_account'])
+        to_account = request.form['to_account']
+        amount = request.form['amount']
+        error = None
+        correct_owner = 0
+
+        if not from_account:
+            error = "From account is required."
+        if not to_account:
+            to_account = "From account is required."
+        if not amount:
+            amount = "Amount is required."
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+
+            db.execute(
+                correct_owner = 'EXISTS(SELECT * FROM account WHERE id = ? AND owner_id = ?' , (from_account, userid?)))
+            db.commit()
+            if correct owner = 1: 
+                db.execute(
+                bal1 = 'SELECT balance FROM account WHERE id = ?' , (from_account, )
+                bal2 = 'SELECT balance FROM account WHERE id = ?' , (from_account, )
+                new_bal1 = bal - amount
+                new_bal2 = bal + amount
+                'UPDATE account'
+                if bal1 > 0: 
+                    'SET balance = ? ', (newbal1, )
+                    'WHERE id = ? ', (to_account, )
+                'SET balance = ? ', (newbal2)
+                'WHERE id = ? ', (from_account))
+
+            return redirect(url_for('bank.index'))
+
+        return render_template('bank/create.html')
